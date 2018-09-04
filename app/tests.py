bdf479ede116b4ab6073db9397415f41ae5122c6
Will Warren <will.warren@groupbyinc.com>
2018-09-04 15:59:34 -0700

    broek it

--- app/tests.py
+++ app/tests.py
@@ -13,15 +13,8 @@ class TestIt(unittest.TestCase):
       print 'start %s' % startResponse
       self.assertEqual(startResponse, '{"color": "red"}')
 
-  def testMove(self):
-    with boddle(body=self.generateMoveRequest1()):
-      moveResponse = move()
-      self.assertEqual(moveResponse, '{"move": "up"}')
-
-  def generateMoveRequest1(self):
-    moveRequest = {}
-    self.addToil(moveRequest)
-    self.convert(moveRequest, 
+  def testMoveInitial(self):
+    with boddle(body=self.generateMoveRequest(
         '____________________' +
         '________2______F____' +
         '____________________' +
@@ -38,10 +31,143 @@ class TestIt(unittest.TestCase):
         '____________F_______' +
         '____________________' +
         '____________________' +
-        '________2___________' +
+        '________3___________' +
         '____________________' +
         '____________F_______' +
-        '____________________'
+        '____________________')):
+      moveResponse = move()
+      self.assertEqual(moveResponse, '{"move": "up"}')
+
+  def testMoveTopWall(self):
+    with boddle(body=self.generateMoveRequest(
+        '___________1________' +
+        '________2______F____' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '___F________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '____________________' +
+        '________3___________' +
+        '____________________' +
+        '____________F_______' +
+        '____________________')):
+      moveResponse = move()
+      self.assertEqual(moveResponse, '{"move": "right"}')
+
+  def testMoveTopRightWall(self):
+    with boddle(body=self.generateMoveRequest(
+        '___________________1' +
+        '________2______F____' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '___F________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '____________________' +
+        '________3___________' +
+        '____________________' +
+        '____________F_______' +
+        '____________________')):
+      moveResponse = move()
+      self.assertEqual(moveResponse, '{"move": "down"}')
+
+  def testMoveBottomRightWall(self):
+    with boddle(body=self.generateMoveRequest(
+        '____________________' +
+        '________2______F____' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '___F________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '____________________' +
+        '________3___________' +
+        '____________________' +
+        '____________F_______' +
+        '___________________1)):
+      moveResponse = move(')
+      self.assertEqual(moveResponse, '{"move": "left"}')
+
+  def testMoveBottomLeftWall(self):
+    with boddle(body=self.generateMoveRequest(
+        '____________________' +
+        '________2______F____' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '___F________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '____________________' +
+        '________3___________' +
+        '____________________' +
+        '____________F_______' +
+        '1___________________')):
+      moveResponse = move()
+      self.assertEqual(moveResponse, '{"move": "left"}')
+
+  def testMoveTopLeftWall(self):
+    with boddle(body=self.generateMoveRequest(
+        '1___________________' +
+        '________2______F____' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '___F________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________________' +
+        '____________F_______' +
+        '____________________' +
+        '____________________' +
+        '________3___________' +
+        '____________________' +
+        '____________F_______' +
+        '____________________')):
+      moveResponse = move()
+      self.assertEqual(moveResponse, '{"move": "right"}')
+
+  def generateMoveRequest(self, asciiBoard):
+    moveRequest = {}
+    self.addToil(moveRequest)
+    self.convert(moveRequest, 
+        asciiBoard
     )
     moveRequest['you'] = {}
     return json.dumps(moveRequest)
@@ -62,7 +188,7 @@ class TestIt(unittest.TestCase):
         self.getOrCreateSnake(asciiPicture[index])['body'].append({ 'y': index /20, 'x': index % 20})    
     
     # help
-    moveRequest['board']['snakes'] = self.snakes
+    moveRequest['board']['snakes'] = self.snakes.values()
     moveRequest['board']['you'] = self.you
     print '%s' % json.dumps(moveRequest)
 
